1f70c5e4c461ac29279544ea6ae35a7dce36092b
bevthedev <bevthedev@gmail.com>
2024-10-23 11:24:24 +1100

    Initial commit rnr

--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function HomeScreen() {
-  return (
-    <View className="p-5">
-      <Text className="text-4xl">Hi</Text>
-    </View>
-  );
-}
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,12 +1,17 @@
-import { Stack } from "expo-router";
-import { View, Text } from "react-native";
+import { Link, Stack } from 'expo-router';
+import { View } from 'react-native';
+import { Text } from '~/components/ui/text';
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: "Oops!" }} />
+      <Stack.Screen options={{ title: 'Oops!' }} />
       <View>
-        <Text>Nooo</Text>
+        <Text>This screen doesn't exist.</Text>
+
+        <Link href='/'>
+          <Text>Go to home screen!</Text>
+        </Link>
       </View>
     </>
   );
